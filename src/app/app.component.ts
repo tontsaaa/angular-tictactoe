46a6c43c140c3a2e1f6fb3eb1c791e617803f9c5
tontsaaa <120172321+tontsaaa@users.noreply.github.com>
2022-12-09 14:44:32 +0200

    Added game logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  button0: string = '';
   button1: string = '';
   button2: string = '';
   button3: string = '';
@@ -14,9 +15,273 @@ export class AppComponent {
   button6: string = '';
   button7: string = '';
   button8: string = '';
-  button9: string = '';
+  array: string[] = ['', '', '', '', '', '', '', '', ''];
+  player: string = 'X';
+  message: string = 'Player 1';
+  gameOver: boolean = false;
 
-  onClick() {
-    console.log('hahaa');
+  constructor() {}
+
+  onClick(index: number) {
+    if (this.array[index] === '') {
+      this.array[index] = this.player;
+      switch (index) {
+        case 0:
+          this.button0 = this.player;
+          if (this.array[1] === this.player && this.array[2] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[3] === this.player && this.array[6] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[4] === this.player && this.array[8] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 1:
+          this.button1 = this.player;
+          if (this.array[0] === this.player && this.array[2] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[4] === this.player && this.array[7] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 2:
+          this.button2 = this.player;
+          if (this.array[0] === this.player && this.array[1] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[4] === this.player && this.array[6] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[5] === this.player && this.array[8] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 3:
+          this.button3 = this.player;
+          if (this.array[0] === this.player && this.array[6] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[4] === this.player && this.array[5] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 4:
+          this.button4 = this.player;
+          if (this.array[0] === this.player && this.array[8] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[2] === this.player && this.array[6] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[1] === this.player && this.array[7] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[3] === this.player && this.array[5] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 5:
+          this.button5 = this.player;
+          if (this.array[2] === this.player && this.array[8] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[3] === this.player && this.array[4] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 6:
+          this.button6 = this.player;
+          if (this.array[0] === this.player && this.array[3] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[4] === this.player && this.array[2] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[7] === this.player && this.array[8] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 7:
+          this.button7 = this.player;
+          if (this.array[4] === this.player && this.array[1] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[6] === this.player && this.array[8] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+        case 8:
+          this.button8 = this.player;
+          if (this.array[4] === this.player && this.array[0] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[5] === this.player && this.array[2] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          if (this.array[7] === this.player && this.array[6] === this.player) {
+            this.gameOver = true;
+            if (this.player === 'X') {
+              this.message = 'Player 1 wins!';
+            } else {
+              this.message = 'Player 2 wins!';
+            }
+          }
+          break;
+      }
+
+      for (let i = 0; i < this.array.length; i++) {
+        if (this.array[i] === '') {
+          break;
+        }
+        if (i === 8) {
+          this.gameOver = true;
+          this.message = 'Draw!';
+        }
+      }
+
+      if (this.gameOver === false) {
+        if (this.player === 'X') {
+          this.player = 'O';
+          this.message = 'Player 2';
+        } else {
+          this.player = 'X';
+          this.message = 'Player 1';
+        }
+      }
+    }
+  }
+
+  reset() {
+    this.button0 = '';
+    this.button1 = '';
+    this.button2 = '';
+    this.button3 = '';
+    this.button4 = '';
+    this.button5 = '';
+    this.button6 = '';
+    this.button7 = '';
+    this.button8 = '';
+    this.array = ['', '', '', '', '', '', '', '', ''];
+    this.player = 'X';
+    this.message = 'Player 1';
+    this.gameOver = false;
   }
 }
